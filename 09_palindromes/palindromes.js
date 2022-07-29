6dd8cab16091df1b64f49cc92951ecee20e0811f
unknown <kurkiewicz.maciej1@gmail.com>
2022-07-29 12:27:20 +0200

    Done the exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let a = word.toLowerCase().replace(/\s/g, '').replace(".", '').replace(/\,/g,"").replace(/!/g,"").split('').join('')
+    let b = word.toLowerCase().replace(/\s/g, '').replace(".", '').replace(/\,/g,"").replace(/!/g,"").split('').reverse().join('')
+    if(a == b) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibonacciNumbers = [1,1];
+    if(typeof num == 'string'){
+        num = parseInt(num);
+    };
+    if(num < 0) {
+        return "OOPS"
+    }
 
+    for(i=1;i<num-1;i++){
+        fibonacciNumbers.push(fibonacciNumbers[i] + fibonacciNumbers[i-1])
+    }
+    return fibonacciNumbers[num-1];
 };
 
 // Do not edit below this line
